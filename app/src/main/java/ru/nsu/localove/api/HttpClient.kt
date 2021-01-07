fb3f58ef89ec9f8b01649d213b2d29023b7fa35c
tokyorider <noobfromrus920@gmail.com>
2021-01-07 22:20:56 +0700

    Начал работу

---
+++ app/src/main/java/ru/nsu/localove/api/HttpClient.kt
@@ -0,0 +1,22 @@
+package ru.nsu.localove.api
+
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.features.*
+import io.ktor.client.features.json.*
+import io.ktor.util.*
+
+@KtorExperimentalAPI
+fun httpClient() = HttpClient(CIO){
+    install(JsonFeature){
+        serializer = JacksonSerializer(JsonMapper.builder().findAndAddModules().build())
+    }
+    disableResponseValidation()
+}
+
+private fun HttpClientConfig<*>.disableResponseValidation(){
+    HttpResponseValidator {
+        validateResponse {  }
+    }
