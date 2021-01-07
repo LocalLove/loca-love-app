d3c9af25a2d6c54486e4812cc9bbe5fcce3ac4f8
tokyorider <noobfromrus920@gmail.com>
2021-01-08 01:25:44 +0700

    Начал работу

--- app/src/main/java/ru/nsu/localove/security/login/LoginFragment.kt
+++ app/src/main/java/ru/nsu/localove/security/login/LoginFragment.kt
@@ -22,6 +22,7 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
