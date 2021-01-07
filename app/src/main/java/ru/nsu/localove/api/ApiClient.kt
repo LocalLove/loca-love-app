3324dacefd4cf78e3e4c7fdebf81dc35e0f6ab40
Tigran Manasyan <manasyan_tigran@mail.ru>
2021-01-08 02:01:19 +0700

    Added TokenService.kt

--- app/src/main/java/ru/nsu/localove/api/ApiClient.kt
+++ app/src/main/java/ru/nsu/localove/api/ApiClient.kt
@@ -9,7 +9,6 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.http.ContentType.Application.Json
 import io.ktor.util.*
-import ru.nsu.localove.security.session.UserSession
 import javax.inject.Inject
 import kotlin.text.Charsets.UTF_8
 
@@ -17,56 +16,57 @@ const val AUTHORIZATION_HEADER = "Authorization"
 const val ACCESS_TOKEN_PREFIX = "Bearer "
 
 class ApiClient @KtorExperimentalAPI @Inject constructor(
-        val http: HttpClient = httpClient(),
-        val baseUrl: String = "localhost",
-        val userSession: UserSession
+    val http: HttpClient = httpClient(),
+    val baseUrl: String = "localhost",
+    val tokenService: TokenService
 ) {
 
     val mapper: JsonMapper = JsonMapper.builder().findAndAddModules().build()
 
     suspend inline fun <reified T> fetch(
-            method: HttpMethod,
-            path: String,
-            body: Any?,
-            headers: Map<String, String>,
-            withAuth: Boolean = true
+        method: HttpMethod,
+        path: String,
+        body: Any?,
+        headers: Map<String, String>,
+        withAuth: Boolean = true
     ): HttpResponse<T> {
         Log.d("Http", "trying to send request: $path, $method, $body")
         val response = httpCall(method, path, body, headers, withAuth)
         val responseBody = response.readText(UTF_8)
         return deserializeHttpResponse(response.status.value, responseBody)
-                ?: error("received error response: $response, body: $responseBody")
+            ?: error("received error response: $response, body: $responseBody")
     }
 
     suspend fun fetchUnit(
-            method: HttpMethod,
-            path: String,
-            body: Any?,
-            headers: Map<String, String>,
-            withAuth: Boolean = true
+        method: HttpMethod,
+        path: String,
+        body: Any?,
+        headers: Map<String, String>,
+        withAuth: Boolean = true
     ): UnitHttpResponse {
         val response = httpCall(method, path, body, headers, withAuth)
         val responseBody = response.readText(UTF_8)
         return deserializeEmptyHttpResponse(response.status.value, responseBody)
-                ?: error("received error response: $response, body: $responseBody")
+            ?: error("received error response: $response, body: $responseBody")
     }
 
     private fun deserializeEmptyHttpResponse(status: Int, body: String): UnitHttpResponse? =
-            when(status) {
-                200 -> UnitHttpResponse.Success
-                400 -> UnitHttpResponse.Error(mapper.readValue(body))
-                else -> null
-            }
+        when (status) {
+            200 -> UnitHttpResponse.Success
+            400 -> UnitHttpResponse.Error(mapper.readValue(body))
+            else -> null
+        }
 
     inline fun <reified T> deserializeHttpResponse(status: Int, body: String): HttpResponse<T>? =
-            when(status) {
-                200 -> HttpResponse.Success(mapper.readValue(body))
-                400 -> HttpResponse.Error(mapper.readValue(body))
-                else -> null
-            }
+        when (status) {
+            200 -> HttpResponse.Success(mapper.readValue(body))
+            400 -> HttpResponse.Error(mapper.readValue(body))
+            else -> null
+        }
 
-    suspend fun httpCall(method: HttpMethod, path: String, body: Any?,
-                         headers: Map<String, String>, withAuth: Boolean = true
+    suspend fun httpCall(
+        method: HttpMethod, path: String, body: Any?,
+        headers: Map<String, String>, withAuth: Boolean = true
     ): io.ktor.client.statement.HttpResponse {
         Log.d("Http", "trying to send request: $path, $method, $body")
         return http.request<HttpStatement>("$baseUrl/${path.trimStart('/')}") {
@@ -76,8 +76,12 @@ class ApiClient @KtorExperimentalAPI @Inject constructor(
             this.headers {
                 headers.forEach(this::append)
                 if (withAuth) {
-                    this.append(AUTHORIZATION_HEADER, ACCESS_TOKEN_PREFIX + userSession.accessToken())
+                    this.append(
+                        AUTHORIZATION_HEADER,
+                        ACCESS_TOKEN_PREFIX + tokenService.accessToken
+                    )
                 }
             }
         }.execute()
-    }}
+    }
+}
