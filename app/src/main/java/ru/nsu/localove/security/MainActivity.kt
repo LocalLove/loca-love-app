d64b18ff09028311dca0e54ea44cd768f5879fea
Tigran Manasyan <manasyan_tigran@mail.ru>
2021-01-07 22:44:23 +0700

    Added hilt support

--- app/src/main/java/ru/nsu/localove/security/MainActivity.kt
+++ app/src/main/java/ru/nsu/localove/security/MainActivity.kt
@@ -2,8 +2,10 @@ package ru.nsu.localove.security
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import ru.nsu.localove.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
