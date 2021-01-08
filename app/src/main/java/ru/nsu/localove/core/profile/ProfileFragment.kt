94c91f1a39a8f914d547252bf385f269b9f85bc2
Tigran Manasyan <manasyan_tigran@mail.ru>
2021-01-08 16:42:53 +0700

    Tiny fix

--- app/src/main/java/ru/nsu/localove/core/profile/ProfileFragment.kt
+++ app/src/main/java/ru/nsu/localove/core/profile/ProfileFragment.kt
@@ -1,14 +1,17 @@
 package ru.nsu.localove.core.profile
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
+import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
 import ru.nsu.localove.R
 import ru.nsu.localove.databinding.FragmentProfileBinding
@@ -40,10 +43,17 @@ class ProfileFragment : Fragment() {
             findNavController(),
             appBarConfiguration
         )
-
+        binding.toolbarLayout.setCollapsedTitleTextColor(Color.WHITE)
+        binding.toolbarLayout.setExpandedTitleColor(Color.WHITE)
         profileViewModel.user.observe(viewLifecycleOwner) {
             binding.toolbar.title = it.login
+            binding.profileUserName.text = it.name
+            binding.profileUserAge.text = it.age.toString()
+            binding.profileUserStatus.text = it.status
 
+            Glide.with(this)
+                .load("http://10.0.2.2:8080/pictures/${it.pictureIds[0]}")
+                .into(binding.detailImage)
         }
 
         binding.detailImage.setImageResource(R.drawable.ic_launcher_background)
